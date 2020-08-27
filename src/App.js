d2f93c90055b35de0f9f797ff093aa8be38fc1c0
SakifKhan98 <61116402+SakifKhan98@users.noreply.github.com>
2020-08-28 05:44:45 +0600

    added post functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React from "react";
 import "./App.css";
+import Post from "./Post";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+
+      {/* Posts */}
+      <Post
+        username="cleverQazi"
+        imageUrl="https://wallpapercave.com/wp/wp4923981.jpg"
+        caption="wow! It Works..!"
+      ></Post>
+
+      <Post
+        username="ssssAghna"
+        imageUrl="https://images.unsplash.com/photo-1598432908102-ce6767f7a264?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=891&q=80"
+        caption="This is really Dope!"
+      ></Post>
+
+      <Post
+        username="amaanath"
+        imageUrl="https://images.unsplash.com/photo-1558980394-4c7c9299fe96?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        caption="Great to see that people are working on this build"
+      ></Post>
+
+      {/* Posts */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Post.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Avatar } from "@material-ui/core";
+import "./Post.css";
+
+function Post({ username, caption, imageUrl }) {
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar
+          className="post__avatar"
+          alt="rafehQazi"
+          src="/static/images/avatar/1.jpg"
+        ></Avatar>
+        <h3>{username}</h3>
+      </div>
+
+      {/* Header --> avatar + username */}
+
+      <img className="post__image" src={imageUrl} alt="" srcset="" />
+
+      {/* image */}
+      <h4 className="post__text">
+        {" "}
+        <strong>{username}</strong> {caption}
+      </h4>
+    </div>
+  );
+}
+
+export default Post;
